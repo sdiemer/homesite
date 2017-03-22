ef6dfe3d84785fd11babf0e218bd67f3e973d6e1
Stéphane Diemer <stephane.diemer@ubicast.eu>
2017-03-22 09:44:03 +0100

    Changed init script.

--- scripts/init.d.sh
+++ scripts/init.d.sh
@@ -2,15 +2,13 @@
 
 ### BEGIN INIT INFO
 # Provides:          homesite
-# Required-Start:    $local_fs $network
-# Required-Stop:     $local_fs $network
+# Required-Start:    $local_fs $network $syslog
+# Required-Stop:     $local_fs $network $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: homesite
 # Description:       homesite management script
 ### END INIT INFO
-
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
 
 # This script should be located in /etc/init.d/homesite
 #   ln -s /home/homesite/homesite/scripts/init.d.sh /etc/init.d/homesite
@@ -25,3 +23,4 @@ if [[ $# < 1 ]]; then
 fi
 
 /bin/su homesite -c "python3 /home/homesite/homesite/scripts/control.py $1"
+exit $?
