d10fe222d558bd1a6e2c8de2ad806ee8548abb31
Stéphane Diemer <stephane.diemer@ubicast.eu>
2020-04-16 18:12:51 +0200

    Flake8 cleaning

--- base/daemons.py
+++ base/daemons.py
@@ -6,14 +6,20 @@ from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
 
-CAN_ACCESS = lambda request: request.user.is_superuser
-CAN_CONTROL = lambda request: request.user.is_superuser
+def CAN_ACCESS(request):
+    return request.user.is_superuser
+
+
+def CAN_CONTROL(request):
+    return request.user.is_superuser
 
 
 DAEMONS = [
-    dict(group='base', name='django', label='Django', no_commands=True, only_log=True,
+    dict(
+        group='base', name='django', label='Django', no_commands=True, only_log=True,
         log_path=os.path.join(settings.TMP_DIR, 'django.log'),
-        help_text=_('This is not a daemon, but only a log file.')),
+        help_text=_('This is not a daemon, but only a log file.')
+    ),
 ]
 GROUPS = [
     dict(name='base', label=_('Base daemons')),
